67738b5eac0d76e73a08d730e36286558c9e0ca2
David Khol <david.khol@linecorp.com>
2022-06-08 23:09:40 +0900

    Add BoardTest

---
+++ core/src/test/kotlin/me/khol/carcassonne/BoardTest.kt
@@ -0,0 +1,31 @@
+package me.khol.carcassonne
+
+import me.khol.carcassonne.tiles.basic.A
+import org.junit.jupiter.api.Test
+import strikt.api.expectThat
+import strikt.assertions.containsExactlyInAnyOrder
+
+internal class BoardTest {
+
+    @Test
+    fun `possible spaces for a tile`() {
+        val board = Board()
+        val newTile = A
+        expectThat(board) {
+            get { possibleSpacesForTile(newTile) }.and {
+                get { keys }.containsExactlyInAnyOrder(
+                    Coordinates(x = +0, y = -1),
+                    Coordinates(x = -1, y = +0),
+                    Coordinates(x = +1, y = +0),
+                )
+                get { values.flatten() }.containsExactlyInAnyOrder(
+                    PlacedTile(Coordinates(x = +0, y = -1), RotatedTile(newTile, Rotation.ROTATE_0)),
+                    PlacedTile(Coordinates(x = +0, y = -1), RotatedTile(newTile, Rotation.ROTATE_90)),
+                    PlacedTile(Coordinates(x = +0, y = -1), RotatedTile(newTile, Rotation.ROTATE_270)),
+                    PlacedTile(Coordinates(x = -1, y = +0), RotatedTile(newTile, Rotation.ROTATE_270)),
+                    PlacedTile(Coordinates(x = +1, y = +0), RotatedTile(newTile, Rotation.ROTATE_90)),
+                )
+            }
+        }
+    }
+}
