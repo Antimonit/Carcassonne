b4fcf9e20447b8e9c48a8c5ad89d791291362346
David Khol <david.khol@linecorp.com>
2022-06-08 23:07:19 +0900

    Update generic Position type for RiverStart and RiverEnd

--- core/src/main/kotlin/me/khol/carcassonne/Element.kt
+++ core/src/main/kotlin/me/khol/carcassonne/Element.kt
@@ -7,9 +7,9 @@ sealed class Element<P : Position> {
     object City : Element<Position.Edge>()
     object Monastery : Element<Position.Center>()
     object Garden : Element<Position.Center>()
-    object RiverStart : Element<Position.Edge>()
+    object RiverStart : Element<Position.Center>()
     object River : Element<Position.Edge>()
-    object RiverEnd : Element<Position.Edge>()
+    object RiverEnd : Element<Position.Center>()
     object CropCircle : Element<Position.Center>()
 
     override fun toString(): String = javaClass.simpleName
