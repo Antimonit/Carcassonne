c0fa47223077a12665f7ffadc9e32dc07cea52bb
David Khol <david.khol@bucketplace.net>
2024-06-03 23:26:05 +0900

    Use data objects

--- core/src/main/kotlin/me/khol/carcassonne/Element.kt
+++ core/src/main/kotlin/me/khol/carcassonne/Element.kt
@@ -2,15 +2,13 @@ package me.khol.carcassonne
 
 sealed class Element<P : Position, G : ElementGroup<P>> {
 
-    object Field : Element<Position.SplitEdge, ElementGroup.Field>()
-    object Road : Element<Position.Edge, ElementGroup.Road>()
-    object City : Element<Position.Edge, ElementGroup.City>()
-    object Monastery : Element<Position.Center, ElementGroup.Center>()
-    object Garden : Element<Position.Center, ElementGroup.Center>()
-    object RiverStart : Element<Position.Center, ElementGroup.Center>()
-    object River : Element<Position.Edge, ElementGroup.Edge>()
-    object RiverEnd : Element<Position.Center, ElementGroup.Center>()
-    object CropCircle : Element<Position.Center, ElementGroup.Center>()
-
-    override fun toString(): String = javaClass.simpleName
+    data object Field : Element<Position.SplitEdge, ElementGroup.Field>()
+    data object Road : Element<Position.Edge, ElementGroup.Road>()
+    data object City : Element<Position.Edge, ElementGroup.City>()
+    data object Monastery : Element<Position.Center, ElementGroup.Center>()
+    data object Garden : Element<Position.Center, ElementGroup.Center>()
+    data object RiverStart : Element<Position.Center, ElementGroup.Center>()
+    data object River : Element<Position.Edge, ElementGroup.Edge>()
+    data object RiverEnd : Element<Position.Center, ElementGroup.Center>()
+    data object CropCircle : Element<Position.Center, ElementGroup.Center>()
 }
