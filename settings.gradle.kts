27eb7023d7cf58acb8e444198a259c89d3d4ff2f
David Khol <david.khol@bucketplace.net>
2024-06-03 23:16:42 +0900

    Add Compose multiplatform

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,9 +1,32 @@
 rootProject.name = "Carcassonne"
+enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
 
-include(":core")
+pluginManagement {
+    repositories {
+        google {
+            mavenContent {
+                includeGroupAndSubgroups("androidx")
+                includeGroupAndSubgroups("com.android")
+                includeGroupAndSubgroups("com.google")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
 
 dependencyResolutionManagement {
     repositories {
+        google {
+            mavenContent {
+                includeGroupAndSubgroups("androidx")
+                includeGroupAndSubgroups("com.android")
+                includeGroupAndSubgroups("com.google")
+            }
+        }
         mavenCentral()
     }
 }
+
+include(":core")
+include(":composeApp")
