d973409dd14c80c0e2e5c2f91f110dae63b1279d
David Khol <david@khol.me>
2025-05-29 21:45:03 +1200

    Add TileEdgesRotationTest

---
+++ core/src/test/kotlin/me/khol/carcassonne/TileEdgesRotationTest.kt
@@ -0,0 +1,41 @@
+package me.khol.carcassonne
+
+import me.khol.carcassonne.tiles.basic.D
+import org.junit.jupiter.api.Test
+import strikt.api.expectThat
+import strikt.assertions.isEqualTo
+
+class TileEdgesRotationTest {
+
+    @Test
+    fun `edge rotation by 0 degrees is idempotent`() {
+        val original = D.edges
+        val rotated = original.rotate(Rotation.ROTATE_0)
+        expectThat(rotated).isEqualTo(original)
+    }
+
+    @Test
+    fun `edge rotation by 90 + 270 degrees is idempotent`() {
+        val original = D.edges
+        val rotated = original.rotate(Rotation.ROTATE_90).rotate(Rotation.ROTATE_270)
+        expectThat(rotated).isEqualTo(original)
+    }
+
+    @Test
+    fun `edge rotation by 180 + 180 degrees is idempotent`() {
+        val original = D.edges
+        val rotated = original.rotate(Rotation.ROTATE_180).rotate(Rotation.ROTATE_180)
+        expectThat(rotated).isEqualTo(original)
+    }
+
+    @Test
+    fun `edge rotation by 90 + 90 + 90 + 90 degrees is idempotent`() {
+        val original = D.edges
+        val rotated = original
+            .rotate(Rotation.ROTATE_90)
+            .rotate(Rotation.ROTATE_90)
+            .rotate(Rotation.ROTATE_90)
+            .rotate(Rotation.ROTATE_90)
+        expectThat(rotated).isEqualTo(original)
+    }
+}
