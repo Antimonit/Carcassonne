593498c66f6f6de3ffa2d4616ae4dfbc8d730ca0
David Khol <david@khol.me>
2025-05-29 23:10:55 +1200

    Add ElementsBuilderTest

---
+++ core/src/test/kotlin/me/khol/carcassonne/ElementsBuilderTest.kt
@@ -0,0 +1,30 @@
+package me.khol.carcassonne
+
+import org.junit.jupiter.api.Test
+import strikt.api.expectThat
+import strikt.assertions.isEqualTo
+
+class ElementsBuilderTest {
+
+    @Test
+    fun `adding elements in two steps is the same as adding in a single step`() {
+        val twoStep = elements {
+            add(
+                Element.City,
+                ElementGroup.city { left },
+                ElementGroup.city { right },
+            )
+        }
+        val singleStep = elements {
+            add(
+                Element.City,
+                ElementGroup.city { left },
+            )
+            add(
+                Element.City,
+                ElementGroup.city { right },
+            )
+        }
+        expectThat(twoStep).isEqualTo(singleStep)
+    }
+}
