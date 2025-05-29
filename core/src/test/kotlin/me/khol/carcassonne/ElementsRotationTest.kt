301ccb934fdba660415da5579e455ab65b180e19
David Khol <david@khol.me>
2025-05-29 21:40:23 +1200

    Add ElementsRotationTest

---
+++ core/src/test/kotlin/me/khol/carcassonne/ElementsRotationTest.kt
@@ -0,0 +1,41 @@
+package me.khol.carcassonne
+
+import me.khol.carcassonne.tiles.basic.D
+import org.junit.jupiter.api.Test
+import strikt.api.expectThat
+import strikt.assertions.isEqualTo
+
+class ElementsRotationTest {
+
+    @Test
+    fun `rotation by 0 degrees is idempotent`() {
+        val original = D.elements
+        val rotated = original.rotate(Rotation.ROTATE_0)
+        expectThat(rotated).isEqualTo(original)
+    }
+
+    @Test
+    fun `rotation by 90 + 270 degrees is idempotent`() {
+        val original = D.elements
+        val rotated = original.rotate(Rotation.ROTATE_90).rotate(Rotation.ROTATE_270)
+        expectThat(rotated).isEqualTo(original)
+    }
+
+    @Test
+    fun `rotation by 180 + 180 degrees is idempotent`() {
+        val original = D.elements
+        val rotated = original.rotate(Rotation.ROTATE_180).rotate(Rotation.ROTATE_180)
+        expectThat(rotated).isEqualTo(original)
+    }
+
+    @Test
+    fun `rotation by 90 + 90 + 90 + 90 degrees is idempotent`() {
+        val original = D.elements
+        val rotated = original
+            .rotate(Rotation.ROTATE_90)
+            .rotate(Rotation.ROTATE_90)
+            .rotate(Rotation.ROTATE_90)
+            .rotate(Rotation.ROTATE_90)
+        expectThat(rotated).isEqualTo(original)
+    }
+}
