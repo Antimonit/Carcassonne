a0f4cc3b0be96b59c90c95122a91bb91e311f075
David Khol <david@khol.me>
2025-06-04 13:59:02 +0900

    Make Coordinates a value class

--- core/src/commonMain/kotlin/me/khol/carcassonne/Coordinates.kt
+++ core/src/commonMain/kotlin/me/khol/carcassonne/Coordinates.kt
@@ -1,18 +1,28 @@
 package me.khol.carcassonne
 
-data class Coordinates(
-    val x: Int,
-    val y: Int,
+@JvmInline
+value class Coordinates private constructor(
+    private val packedValue: Long,
 ) {
-    val top: Coordinates by lazy { copy(y = y - 1) }
-    val bottom: Coordinates by lazy { copy(y = y + 1) }
-    val left: Coordinates by lazy { copy(x = x - 1) }
-    val right: Coordinates by lazy { copy(x = x + 1) }
-    val neighbors: List<Coordinates> by lazy { listOf(top, bottom, left, right) }
+
+    constructor(x: Int, y: Int) : this((x.toLong() shl 32) or (y.toLong() and 0xFFFFFFFF))
+
+    val x: Int
+        get() = (packedValue shr 32).toInt()
+    val y: Int
+        get() = (packedValue and 0xFFFFFFFF).toInt()
 
     override fun toString(): String {
         return "[$x, $y]"
     }
+
+    fun copy(x: Int = this.x, y: Int = this.y) = Coordinates(x = x, y = y)
+
+    val top: Coordinates inline get() = copy(y = y - 1)
+    val bottom: Coordinates inline get() = copy(y = y + 1)
+    val left: Coordinates inline get() = copy(x = x - 1)
+    val right: Coordinates inline get() = copy(x = x + 1)
+    val neighbors: List<Coordinates> get() = listOf(top, bottom, left, right)
 }
 
 /**
