e223c5ca7b0a0b93bb8e5d82bc7962df2cc1c026
David Khol <david@khol.me>
2025-06-05 12:10:32 +0900

    Add Coordinates.oppositeCoordinates

--- core/src/main/kotlin/me/khol/carcassonne/Coordinates.kt
+++ core/src/main/kotlin/me/khol/carcassonne/Coordinates.kt
@@ -25,3 +25,23 @@ fun Coordinates.surroundingCoordinates(): List<Coordinates> {
         }
     }.flatten()
 }
+
+fun Coordinates.oppositeCoordinates(edge: ElementPosition.Edge): Coordinates =
+    when (edge) {
+        ElementPosition.Edge.Top -> top
+        ElementPosition.Edge.Right -> right
+        ElementPosition.Edge.Bottom -> bottom
+        ElementPosition.Edge.Left -> left
+    }
+
+fun Coordinates.oppositeCoordinates(edge: ElementPosition.SplitEdge): Coordinates =
+    when (edge) {
+        ElementPosition.SplitEdge.TopLeft -> top
+        ElementPosition.SplitEdge.TopRight -> top
+        ElementPosition.SplitEdge.RightTop -> right
+        ElementPosition.SplitEdge.RightBottom -> right
+        ElementPosition.SplitEdge.BottomLeft -> bottom
+        ElementPosition.SplitEdge.BottomRight -> bottom
+        ElementPosition.SplitEdge.LeftTop -> left
+        ElementPosition.SplitEdge.LeftBottom -> left
+    }
