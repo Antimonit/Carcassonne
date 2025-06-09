aff4c42538257b5d51413b97f0632cb0b7ab697a
David Khol <david@khol.me>
2025-06-09 20:19:08 +0900

    Add rotation to desktop implementation of PanningWindow

--- composeApp/src/desktopMain/kotlin/me/khol/carcassonne/common/PanningWindow.desktop.kt
+++ composeApp/src/desktopMain/kotlin/me/khol/carcassonne/common/PanningWindow.desktop.kt
@@ -1,11 +1,13 @@
 package me.khol.carcassonne.common
 
 import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.PointerMatcher
 import androidx.compose.foundation.gestures.detectDragGestures
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableFloatStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
@@ -15,11 +17,14 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.TransformOrigin
 import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.input.pointer.PointerButton
 import androidx.compose.ui.input.pointer.PointerEventType
 import androidx.compose.ui.input.pointer.onPointerEvent
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.unit.center
 import androidx.compose.ui.unit.toOffset
+import kotlin.math.PI
+import kotlin.math.atan2
 
 @Composable
 actual fun PanningWindow(
@@ -27,6 +32,34 @@ actual fun PanningWindow(
 ) {
     var zoom by remember { mutableStateOf(1f) }
     var pan by remember { mutableStateOf(Offset.Zero) }
+    var rotate by remember { mutableFloatStateOf(0f) }
+
+    val mouseRotateModifier = Modifier
+        .pointerInput(Unit) {
+            fun Offset.toAngle(): Float = atan2(x, y) / PI.toFloat() / 2 * 360
+
+            var previousOffset: Offset = Offset.Unspecified
+            var angle = 0f
+
+            @OptIn(ExperimentalFoundationApi::class)
+            detectDragGestures(
+                matcher = PointerMatcher.mouse(PointerButton.Tertiary),
+                onDragStart = {
+                    previousOffset = it
+                    val centroid = size.center.toOffset()
+                    angle = (centroid - previousOffset).toAngle()
+                },
+                onDrag = { dragAmount: Offset ->
+                    previousOffset += dragAmount
+                    val centroid = size.center.toOffset()
+                    val polarAngle = (centroid - previousOffset).toAngle()
+                    val gestureRotate = (angle - polarAngle)
+                    angle = polarAngle
+                    pan = pan.rotateBy(gestureRotate)
+                    rotate += gestureRotate
+                }
+            )
+        }
 
     val mousePanModifier = Modifier
         .pointerInput(Unit) {
@@ -55,6 +88,7 @@ actual fun PanningWindow(
         contentAlignment = Alignment.Center,
         modifier = Modifier
             .fillMaxSize()
+            .then(mouseRotateModifier)
             .then(mouseZoomModifier)
             .then(mousePanModifier)
             .graphicsLayer {
@@ -62,6 +96,7 @@ actual fun PanningWindow(
                 translationY = -pan.y * zoom
                 scaleX = zoom
                 scaleY = zoom
+                rotationZ = rotate
                 transformOrigin = TransformOrigin(0.5f, 0.5f)
             }
     ) {
