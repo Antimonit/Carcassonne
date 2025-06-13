bbddfc3ea5a9476c516554db91c4904b7298af85
David Khol <david@khol.me>
2025-06-13 15:20:51 +0900

    Add Offset.rotateBy extension

--- composeApp/src/commonMain/kotlin/me/khol/carcassonne/common/PanningWindow.kt
+++ composeApp/src/commonMain/kotlin/me/khol/carcassonne/common/PanningWindow.kt
@@ -1,6 +1,17 @@
 package me.khol.carcassonne.common
 
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.geometry.Offset
+import kotlin.math.PI
+import kotlin.math.cos
+import kotlin.math.sin
+
+fun Offset.rotateBy(angle: Float): Offset {
+    val angleInRadians = angle * (PI / 180)
+    val cos = cos(angleInRadians)
+    val sin = sin(angleInRadians)
+    return Offset((x * cos - y * sin).toFloat(), (x * sin + y * cos).toFloat())
+}
 
 @Composable
 expect fun PanningWindow(
