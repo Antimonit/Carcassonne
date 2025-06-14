315670875a7d10e33d889f21371925189f611a5a
David Khol <david@khol.me>
2025-06-14 20:21:05 +0900

    Add Board to App

--- composeApp/src/commonMain/kotlin/me/khol/carcassonne/common/App.kt
+++ composeApp/src/commonMain/kotlin/me/khol/carcassonne/common/App.kt
@@ -2,8 +2,12 @@ package me.khol.carcassonne.common
 
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import me.khol.carcassonne.Board
+import me.khol.carcassonne.Coordinates
+import me.khol.carcassonne.RotatedTile
+import me.khol.carcassonne.Rotation
+import me.khol.carcassonne.tiles.basic.D
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
 @Preview
@@ -14,8 +18,15 @@ fun App() {
             color = MaterialTheme.colors.background,
         ) {
             PanningWindow {
-                Text(
-                    text = "Hello",
+                Board(
+                    board = Board
+                        .starting(startingTile = D)
+                        .placeTile(coordinates = Coordinates(-1, 0), tile = RotatedTile(D, Rotation.ROTATE_180))
+                        .placeTile(coordinates = Coordinates(-2, 0), tile = RotatedTile(D, Rotation.ROTATE_180))
+                        .placeTile(coordinates = Coordinates(-3, 0), tile = RotatedTile(D, Rotation.ROTATE_180))
+                        .placeTile(coordinates = Coordinates(1, 0), tile = RotatedTile(D, Rotation.ROTATE_180))
+                        .placeTile(coordinates = Coordinates(1, -1), tile = RotatedTile(D, Rotation.ROTATE_0))
+                        .placeTile(coordinates = Coordinates(0, 1), tile = RotatedTile(D, Rotation.ROTATE_180))
                 )
             }
         }
