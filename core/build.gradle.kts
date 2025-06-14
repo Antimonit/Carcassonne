86accecf654dabded1e05f0ede711fc0eaec4a04
David Khol <david@khol.me>
2025-06-14 14:26:40 +0900

    Add power-assert plugin

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     alias(libs.plugins.kotlin.multiplatform)
+    alias(libs.plugins.kotlin.powerAssert)
     alias(libs.plugins.kover)
 }
 
@@ -15,4 +16,14 @@ kotlin {
 
 tasks.withType<Test>().configureEach {
     useJUnitPlatform()
+}
+
+powerAssert {
+    functions.addAll(
+        "kotlin.test.assertEqAAuals",
+        "kotlin.test.assertTrue",
+        "kotlin.test.assertFalse",
+        "kotlin.assert",
+        "kotlin.require",
+    )
 }
