a1ba4aad917e8a5e8836cadd99fb7d931ecd0a59
David Khol <david@khol.me>
2025-07-18 11:23:53 +0900

    Make ElementKey an interface

--- core/src/commonMain/kotlin/me/khol/carcassonne/ElementKey.kt
+++ core/src/commonMain/kotlin/me/khol/carcassonne/ElementKey.kt
@@ -1,14 +1,14 @@
 package me.khol.carcassonne
 
-open class ElementKey<E : Element<ElementPosition>> {
+interface ElementKey<E : Element<ElementPosition>> {
 
-    data object Field : ElementKey<Element.Field>()
-    data object Road : ElementKey<Element.Road>()
-    data object City : ElementKey<Element.City>()
-    data object Monastery : ElementKey<Element.Monastery>()
-    data object Garden : ElementKey<Element.Garden>()
-    data object RiverStart : ElementKey<Element.RiverStart>()
-    data object River : ElementKey<Element.River>()
-    data object RiverEnd : ElementKey<Element.RiverEnd>()
-    data object CropCircle : ElementKey<Element.CropCircle>()
+    data object Field : ElementKey<Element.Field>
+    data object Road : ElementKey<Element.Road>
+    data object City : ElementKey<Element.City>
+    data object Monastery : ElementKey<Element.Monastery>
+    data object Garden : ElementKey<Element.Garden>
+    data object RiverStart : ElementKey<Element.RiverStart>
+    data object River : ElementKey<Element.River>
+    data object RiverEnd : ElementKey<Element.RiverEnd>
+    data object CropCircle : ElementKey<Element.CropCircle>
 }
