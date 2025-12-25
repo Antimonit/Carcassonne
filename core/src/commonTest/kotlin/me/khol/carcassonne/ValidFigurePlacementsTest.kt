7d84623fb5d5039c611c73af08f61bf68e9d230b
David Khol <david@khol.me>
2025-12-26 02:04:21 +0900

    Add a test to ValidFigurePlacementsTest

--- core/src/commonTest/kotlin/me/khol/carcassonne/ValidFigurePlacementsTest.kt
+++ core/src/commonTest/kotlin/me/khol/carcassonne/ValidFigurePlacementsTest.kt
@@ -1,6 +1,7 @@
 package me.khol.carcassonne
 
 import me.khol.carcassonne.feature.placed
+import me.khol.carcassonne.figure.Abbot
 import me.khol.carcassonne.figure.Meeple
 import me.khol.carcassonne.fixtures.PlayerFigures
 import me.khol.carcassonne.fixtures.Players
@@ -51,6 +52,44 @@ class ValidFigurePlacementsTest {
                         figure = PlayerFigures.greenMeeple,
                     ),
                 ),
+            ),
+            actual = placements,
+        )
+    }
+
+    @Test
+    fun `no more than 7 meeples can be placed by a single player`() {
+        var board = Board.starting(startingTile = Tiles.Basic.D.tile)
+
+        repeat(7) { x ->
+            val coordinates = Coordinates(x + 1, 0)
+            board = board.placeTile(
+                coordinates = coordinates,
+                tile = Tiles.Basic.D.tile.rotated(Rotation.ROTATE_0),
+                placedFigures = listOf(
+                    PlacedFigure(
+                        placedElement = Tiles.Basic.D.city.rotated(Rotation.ROTATE_0).placed(coordinates),
+                        figure = PlayerFigures.greenMeeple,
+                    ),
+                ),
+            )
+        }
+
+        val placements = board.validFigurePlacements(
+            placedTile = Tiles.Basic.D.tile.rotated(Rotation.ROTATE_0).placed(8, 0),
+            currentPlayer = Players.green,
+            figureSupply = board.figureSupply(
+                players = listOf(Players.green),
+                maxFigureCounts = mapOf(Meeple to 7, Abbot to 1),
+            ),
+        )
+
+        assertEquals(
+            expected = mapOf(
+                Tiles.Basic.D.fieldTop.rotated(Rotation.ROTATE_0) to emptyList(),
+                Tiles.Basic.D.fieldBottom.rotated(Rotation.ROTATE_0) to emptyList(),
+                Tiles.Basic.D.road.rotated(Rotation.ROTATE_0) to emptyList(),
+                Tiles.Basic.D.city.rotated(Rotation.ROTATE_0) to emptyList(),
             ),
             actual = placements,
         )
