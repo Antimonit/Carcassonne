5087383fa9185ac20dc27818ed50607d08153860
David Khol <david@khol.me>
2026-03-15 15:04:42 +0900

    Move FeatureStringBuilder to a separate file

--- core/src/commonMain/kotlin/me/khol/carcassonne/feature/Feature.kt
+++ core/src/commonMain/kotlin/me/khol/carcassonne/feature/Feature.kt
@@ -117,37 +117,5 @@ sealed interface Feature {
     }
 }
 
-private interface FeatureStringBuilder {
-    fun <T> withField(name: String, collection: Collection<T>): FeatureStringBuilder
-    fun withField(name: String, any: Any): FeatureStringBuilder
-}
-
-private class FeatureStringBuilderImpl : FeatureStringBuilder {
-
-    private val fields = mutableListOf<String>()
-
-    override fun <T> withField(name: String, collection: Collection<T>): FeatureStringBuilder = apply {
-        val array = if (collection.isEmpty()) {
-            "[]"
-        } else {
-            collection.joinToString("\n", prefix = "[\n", postfix = "\n]") { "    $it," }
-        }
-        fields += "$name = $array"
-    }
-
-    override fun withField(name: String, any: Any): FeatureStringBuilder = apply {
-        fields += "$name = $any"
-    }
-
-    override fun toString(): String = fields.joinToString(separator = ",\n", postfix = ",")
-}
-
-private fun buildFeatureString(name: String, builderAction: FeatureStringBuilder.() -> Unit): String = buildString {
-    append(name)
-    appendLine("(")
-    appendLine(FeatureStringBuilderImpl().apply(builderAction).toString().prependIndent("    "))
-    append(")")
-}
-
 infix operator fun Feature.contains(placedFigure : PlacedFigure): Boolean =
     placedFigure.placedElement in placedElements
