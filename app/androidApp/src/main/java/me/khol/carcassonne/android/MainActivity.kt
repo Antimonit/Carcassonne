3a31a3681510e8e342fa0a59aadbfaab2c5c165d
David Khol <david@khol.me>
2026-05-31 16:53:58 +0900

    Make status bar contents black

--- app/androidApp/src/main/java/me/khol/carcassonne/android/MainActivity.kt
+++ app/androidApp/src/main/java/me/khol/carcassonne/android/MainActivity.kt
@@ -2,6 +2,7 @@ package me.khol.carcassonne.android
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.runtime.Composable
@@ -10,7 +11,12 @@ import me.khol.carcassonne.ui.App
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        enableEdgeToEdge()
+        enableEdgeToEdge(
+            statusBarStyle = SystemBarStyle.light(
+                scrim = android.graphics.Color.TRANSPARENT,
+                darkScrim = android.graphics.Color.TRANSPARENT,
+            ),
+        )
         super.onCreate(savedInstanceState)
 
         setContent {
